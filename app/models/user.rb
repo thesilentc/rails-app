300fb82772557aa1be5355f7ca9198f5fcea0c18
James Borocz <thesilentc@me.com>
2019-06-03 17:06:57 -0400

    create omniauth initializers

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,32 @@
 class User < ActiveRecord::Base
+  validates :first_name, presence: true, length: { minimum: 3 }
+  validates :last_name, presence: true, length: { minimum: 2 }
+
+  validates :password, presence: true
+
+  validates :email, presence: true, uniqueness: true
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
+
+
+  has_secure_password
+  
   has_many :hosts
   has_many :hats, through: :hosts
 
-   has_secure_password
-
-   def user
-   end
-
+  def self.from_omniauth(auth)
+    where(auth.slice("provider", "uid")).first || create_from_omniauth(auth)
+  end
+  
+  def self.create_from_omniauth(auth)
+    create! do |user|     
+      user.provider = auth["provider"]
+      user.uid = auth["uid"]
+      user.first_name = auth["info"]["nickname"]
+      user.last_name = auth["info"]["name"]
+      # user.email = auth["info"]["email"]
+      user.email = user.first_name + "@pgm.com"
+      user.password = SecureRandom.hex
+    end
+  end
+  
 end
