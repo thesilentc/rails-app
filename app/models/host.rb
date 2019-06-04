d8394a503564c17a8e256efe88c3c9b055cf50b6
James Borocz <thesilentc@me.com>
2019-06-04 10:44:40 -0400

    Host views

--- app/models/host.rb
+++ app/models/host.rb
@@ -1,5 +1,10 @@
 class Host < ActiveRecord::Base
+  validates :name, presence: true, uniqueness: true
 
   has_many :users
   belongs_to :hat
+
+  scope :status, -> { where(status: 'active') }
+  scope :recent, -> { order('projects.updated_at DESC') }
+
 end
