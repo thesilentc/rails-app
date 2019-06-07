953134f4a0d3b82ffdd6e49ae6f341513f7085be
James Borocz <thesilentc@me.com>
2019-06-06 22:45:08 -0400

    test new CreatHosts migration

--- db/migrate/20190531012608_create_hosts.rb
+++ db/migrate/20190531012608_create_hosts.rb
@@ -1,11 +1,12 @@
 class CreateHosts < ActiveRecord::Migration[5.2]
   def change
     create_table :hosts do |t|
-      t.string :name
-      t.string :description
-      t.string :status
+      t.string :content            
+      t.integer :hat_id
+      t.integer :user_id
+      t.datetime :completed_at
 
-      t.timestamps
+      t.timestamps  
     end
   end
 end
